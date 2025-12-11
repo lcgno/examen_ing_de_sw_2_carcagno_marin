113233f6a8c03ebf0aa3a36b80ba48e10ec0c85f
lcgno <carcagnoleandro@gmail.com>
2025-12-11 00:11:04 -0300

    otro mart

--- dbt/models/marts/fct_customer_transactions.sql
+++ dbt/models/marts/fct_customer_transactions.sql
@@ -4,3 +4,11 @@ with base as (
 )
 
 -- TODO: Completar el modelo para que cree la tabla fct_customer_transactions con las metricas en schema.yml.
+select
+    cast(customer_id as int) as customer_id,
+    count(*) as transaction_count,
+    sum(amount) as total_amount_completed,
+    sum(amount) as total_amount_all
+from base
+where status = 'completed'
+group by customer_id
